0ed4c016c46c1f299a3b04816fe56fc4303dab4f
Blaine Johnson <admin@digifund.org>
2019-11-28 03:54:25 +0000

    Plugins

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit1c263c428b5a36b29acfa1b7347f103a::getLoader();
+return ComposerAutoloaderInitabfc8685fac717f98c336d72034f94db::getLoader();
